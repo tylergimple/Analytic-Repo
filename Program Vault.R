9a4887fadc8632cb3552ef5f81434fefff5a3646
tylergimple <59339453+tylergimple@users.noreply.github.com>
2022-02-03 13:07:14 -0500

    Update Program Vault.R

--- Program Vault.R
+++ Program Vault.R
@@ -1,6 +1,6 @@
-#This is Weber Shandwick's Program Vault.
-#Feel free to use any of the templates built out here in your projects.
-#If you would like to add a template of your own for another WS friend to use please contact tgimple@webershandwick.com and I will add it to the vault :)
+#Hello Welcome to Weber Shandwick's Program Vault.
+#Feel free to use any of the templates here in your projects.
+#If you would like to add a template or program of your own for another WS friend to use please contact tgimple@webershandwick.com and I will add it to the vault :)
 
 
 #Make sure these packages are installed and loaded.
@@ -17,11 +17,11 @@ Data<-as.data.frame(read.csv("Data.csv"))
 #Remove any row or column if desired, e.g. removing outliers. This removes the 1st and 2nd row.
 Data<-as.data.frame(Data[-c(1,2),])
 
-#Simple Bubble chart of 4 dimensional data. Update x, y, size, color, label to desired values.
+#Simple Bubble chart of 4 dimensional data. Update x, y, size, color, label to desired variables
 ggplot(Data, aes(x='xaxis', y='yaxis', size='sizemetric', color='colormetric')) + geom_point(alpha=0.5) + 
   geom_text(label='label', nudge_x = 0.25, nudge_y = 0.25, check_overlap = T)+theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
 
-#Plotly Bubble Chart (more interactive and sightly)
+#Plotly Bubble Chart (more interactive and sightly). Update x, y, size, color to desired variables. Be sure to keep the ~ to preserve plotly's syntax.
 bubbleplot2 <- plot_ly(Data, x = ~xaxis, y = ~yaxis,
                        text = ~text, size = ~sizemetric,
                        sizes = c(10, 50),
@@ -33,19 +33,19 @@ bubbleplot2
 
 
 
-#Simple Bar Chart
+#Simple Bar Chart. Update x, y to desired variables.
 ggplot(data=DemoData, aes(x=Name, y=Income)) +
   geom_bar(stat="identity", width=0.6,fill="purple")+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
-#Correlegram, Remember to remove non-numeric columns
+#Correlegram, Remember to remove non-numeric columns. This is useful to understand the level of correlation between two or more variables.
 CorData<-as.data.frame(subset(Data, select = -c(non-numeric1,non-numeric2)))
 corrplot(Data, is.corr = FALSE, method = "circle")
 M<-cor(CorData)
 corrplot(M, method="number")
 pairs(~Metric1 + Metric2 + Metric3 + Metric4 + Metric5, data = Data)
 
-#Graphs with Animation. Useful to graph the movement of data over time.
+#Graphs with Animation. Useful to graph the movement of data over time. E.G. how has a group of influencers changed over time in terms of followers and engagement.
 Data <- Data #Load your data here
 fig <- Data %>%
   plot_ly(
@@ -66,8 +66,8 @@ fig <- fig %>% layout(
 )
 fig
 
-#Text Analytics. This is useful if you have a large amount of text and want to see what the most common words being used are.
-#Make sure these packages are loaded.
+#Text Analytics. This is useful if you have a large amount of text and want to see what the most common words being used are. E.G. you want to see the most common
+#words from every Glassdoor post. Make sure these packages are loaded.
 library(wordcloud)
 library(wordcloud2)
 library(tm)
